ebd677ef0399664c3908a9c579b1d2d7300f308b
Marco Antonio Rodriguez Salinas <marcorodriguez2013@outlook.com>
2022-09-20 17:52:29 -0500

    first commit

---
+++ ejemplo.js
@@ -0,0 +1,113 @@
+const { Client, Location, List, Buttons, LocalAuth } = require('./index')
+const fs = require('fs')
+const converBase64ToImage = require('convert-base64-to-image')
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { headless: false }
+});
+
+client.initialize();
+
+client.on('loading_screen', (percent, message) => {
+    console.log('LOADING SCREEN', percent, message);
+});
+
+client.on('qr', (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessful
+    console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('ready', () => {
+    console.log('READY');
+});
+
+client.on('message', async msg => {
+    // console.log('MESSAGE RECEIVED', msg);
+    const { from, to, body, hasMedia, mediaKey } = msg
+    if (hasMedia) {
+        const mediafile = await msg.downloadMedia()
+        // console.log(
+        //     mediafile.mimetype,
+        //     mediafile.filename,
+        //     mediafile.data.length
+        // )
+
+        fs.writeFile(`./upload/${msg.from}-${msg.to}.txt`, `data:${mediafile.mimetype};base64,${mediafile.data}`, function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+
+});
+
+client.on('message_create', (msg) => {
+    // Fired on all message creations, including your own
+    if (msg.fromMe) {
+        // do stuff here
+    }
+});
+
+client.on('message_revoke_everyone', async (after, before) => {
+    // Fired whenever a message is deleted by anyone (including you)
+    console.log(after); // message after it was deleted.
+    if (before) {
+        console.log(before); // message before it was deleted.
+    }
+});
+
+client.on('message_revoke_me', async (msg) => {
+    // Fired whenever a message is only deleted in your own view.
+    console.log(msg.body); // message before it was deleted.
+});
+
+client.on('message_ack', (msg, ack) => {
+    /*
+        == ACK VALUES ==
+        ACK_ERROR: -1
+        ACK_PENDING: 0
+        ACK_SERVER: 1
+        ACK_DEVICE: 2
+        ACK_READ: 3
+        ACK_PLAYED: 4
+    */
+
+    if (ack == 3) {
+        // The message was read
+    }
+});
+
+client.on('group_join', (notification) => {
+    // User has joined or been added to the group.
+    console.log('join', notification);
+    notification.reply('User joined.');
+});
+
+client.on('group_leave', (notification) => {
+    // User has left or been kicked from the group.
+    console.log('leave', notification);
+    notification.reply('User left.');
+});
+
+client.on('group_update', (notification) => {
+    // Group picture, subject or description has been updated.
+    console.log('update', notification);
+});
+
+client.on('change_state', state => {
+    console.log('CHANGE STATE', state);
+});
+
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
+
